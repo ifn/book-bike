75aa347b92f5aaf3e53cb31f67686f8d9e75c173
ifn <shabaev.shat@gmail.com>
2015-08-30 02:07:52 +0300

    bone idea

--- main.go
+++ main.go
@@ -2,18 +2,48 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-
-	"github.com/codegangsta/negroni"
 )
 
-func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "book your bike order here")
-	})
+func getAutoOffers(model string) <-chan string {
+	links := make(chan string)
+	go func() {
+		links <- "http://moto.auto.ru/motorcycle/used/sale/1826802-02dbed.html"
+		close(links)
+	}()
+	return links
+}
 
-	n := negroni.Classic()
-	n.UseHandler(mux)
-	n.Run(":3344")
+func getAvitoOffers(model string) <-chan string {
+	links := make(chan string)
+	go func() {
+		links <- "https://www.avito.ru/moskva/mototsikly_i_mototehnika/honda_cbr_1000_rr_fireblade_632522473"
+		close(links)
+	}()
+	return links
+}
+
+func getOffers(model string) {
+	auto := getAutoOffers(model)
+	avito := getAvitoOffers(model)
+
+	for auto != nil || avito != nil {
+		select {
+		case link, ok := <-auto:
+			if !ok {
+				auto = nil
+				break
+			}
+			fmt.Println(link)
+		case link, ok := <-avito:
+			if !ok {
+				avito = nil
+				break
+			}
+			fmt.Println(link)
+		}
+	}
+}
+
+func main() {
+	getOffers("VFR800")
 }
